c8f42469085fdc0345e50c9a0d713e2bab3f97af
Victor Landa <victorlanda.dev@gmail.com>
2024-05-25 17:50:56 -0600

    nest logging middleware

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,15 +7,18 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common'
 import { EmployeesService } from './employees.service'
 import { Prisma } from '@prisma/client'
 import { SkipThrottle, Throttle } from '@nestjs/throttler'
+import { MyLoggerService } from 'src/my-logger/my-logger.service'
 
 @SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new MyLoggerService(EmployeesController.name)
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
@@ -26,7 +29,14 @@ export class EmployeesController {
     default: false,
   })
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  findAll(
+    @Ip() ip: string,
+    @Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN',
+  ) {
+    this.logger.log(
+      `Request for all employees \t ${ip}`,
+      EmployeesController.name,
+    )
     return this.employeesService.findAll(role)
   }
 
